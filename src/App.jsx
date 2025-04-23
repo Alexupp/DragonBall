d6095280deddce61edaeefaf4e0017ab7dceecbd
Alexupp <alexpecomacecomo@gmail.com>
2025-04-23 09:16:39 -0500

    Manejo de errores

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import MasculinoPage from './Pages/MasculinoPage/MasculinoPage';
 import FemeninoPage from './Pages/FemeninoPage/FemeninoPage';
 import AcercaPage from './Pages/AcercaDePage/AcercaDePage';
 import PersonajePage from './Pages/PersonajePage/PersonajePage';
+import NotFoundPage from './Pages/NotFoundPage/NotFoundPage';
 
 function App() {
   return (
@@ -13,9 +14,12 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/masculino" element={<MasculinoPage />} />
+        <Route path="/masculino/*" element={<NotFoundPage />} /> {/* Para rutas mal escritas dentro de /masculino */}
         <Route path="/femenino" element={<FemeninoPage />} />
+        <Route path="/femenino/*" element={<NotFoundPage />} />
         <Route path="/acerca" element={<AcercaPage />} />
         <Route path="/personaje/:id" element={<PersonajePage />} />
+        <Route path="*" element={<NotFoundPage />} /> {/* Catch-all final */}
       </Routes>
     </Router>
   );
---
+++ src/Pages/NotFoundPage/NotFoundpage.jsx
@@ -0,0 +1,28 @@
+import "./NotFoundPage.css";
+import { useNavigate } from 'react-router-dom';
+
+function NotFoundPage() {
+    const navigate = useNavigate();
+
+    return (
+        <div className="not-found-container">
+            <h1 className="not-found-title">404 - Página no encontrada</h1>
+            <p className="not-found-text">La ruta que intentas acceder no existe.</p>
+            
+            <img 
+                src="https://i.pinimg.com/originals/7a/8b/e1/7a8be12b3f3384ebce47064ab728282c.gif" 
+                alt="Página no encontrada"
+                className="not-found-gif"
+            />
+            
+            <button
+                className="back-button"
+                onClick={() => navigate('/')}
+            >
+                Volver al inicio
+            </button>
+        </div>
+    );
+}
+
+export default NotFoundPage;
